2581d93ea9f81d9a86e3c1e2269eb824b5be5b91
Rahul <rahulsuthar90243@gmail.com>
2026-04-18 19:11:49 +0530

    Solved Leetcode Problem 443. String Compression

---
+++ BinarySearchAlgorithm/StringLeetcodeProblem/443StringCompression.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int compression(vector<char> &chars){
+    int n = chars.size();
+    int idx = 0;
+    for (int i = 0; i < n; i++)
+    {
+        char ch = chars[i];
+        int count = 0;
+        
+        while (i < n && chars[i] == ch)
+        {
+            count++, i++;
+        }
+        
+        if(count == 1){
+            chars[idx++] = ch;
+        }else{
+            chars[idx++] = ch;
+            string str = to_string(count);
+            for(char dig: str){
+                chars[idx++] = dig;
+            }
+        }
+        i--;
+    }
+    chars.reserve(idx);
+    return idx;
+}
+
+int main(){
+ 
+    vector<char> chars = {'a','a','b','b','c','c','c'};
+
+    cout<<compression(chars)<<endl;
+
+    for(char ch: chars){
+        cout<< ch<<" ";
+    }
+}
