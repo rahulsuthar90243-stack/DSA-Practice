1bf2b40c4676c7d4ea3fbda3016dd28133b126c4
Rahul <rahulsuthar90243@gmail.com>
2026-04-21 20:34:44 +0530

    Find the Duplicate Number solved leetcode 287

---
+++ Hash_Table/findTheDuplicateNumber.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int findDupicate(vector<int> &nums){
+    int slow = nums[0], fast = nums[0];
+    do
+    {
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+    } while (slow != fast);
+
+    slow = nums[0];
+    while (slow != fast)
+    {
+        slow = nums[slow];
+        fast = nums[fast];
+    }
+
+    return slow;
+}
+
+int main(){
+    vector<int> nums = {1, 3, 4, 2, 2};
+    cout<<findDupicate(nums);
+
+}
