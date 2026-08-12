99c1e2f81358fd6a44ccf02c6aa0787c19afd987
Rahul <rahulsuthar90243@gmail.com>
2026-08-12 19:15:18 +0530

    Add Quick Sort implementation

---
+++ recursion/Quick_sort.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int partition(vector<int> &arr, int st, int end) {
+    int idx = st - 1;
+    int pivot = arr[end];
+
+    for (int j = st; j < end; j++) {
+        if (arr[j] <= pivot) {
+            idx++;
+            swap(arr[j], arr[idx]);
+        }
+    }
+
+    idx++;
+    swap(arr[end], arr[idx]);
+
+    return idx;
+}
+
+void quickSort(vector<int> &arr, int st, int end) {
+    if (st < end) {
+        int pivot = partition(arr, st, end);
+
+        quickSort(arr, st, pivot - 1);
+        quickSort(arr, pivot + 1, end);
+    }
+}
+
+int main() {
+    vector<int> arr = {5, 2, 6, 4, 1, 3};
+
+    quickSort(arr, 0, arr.size() - 1);
+
+    for (int val : arr) {
+        cout << val << " ";
+    }
+
+    cout << endl;
+
+    return 0;
+}
